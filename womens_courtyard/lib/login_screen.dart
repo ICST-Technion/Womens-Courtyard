61a75025f3ec67b8192264a6163cd4f763f68d1a
Itamar Juwiler <itamarj@campus.technion.ac.il>
2022-01-20 15:43:10 +0200

    Added Login UI

---
+++ womens_courtyard/lib/login_screen.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'register_screen.dart' as registration_screen;
+import 'BottomNavigationBar.dart' as bottom_navigation_bar;
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  //form key
+  final _formKey = GlobalKey<FormState>();
+
+  //controller
+  final TextEditingController emailController = new TextEditingController();
+  final TextEditingController passController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    //email field
+    final emailField = TextFormField(
+        autofocus: false,
+        controller: emailController,
+        keyboardType: TextInputType.emailAddress,
+        //validator: () {},
+        onSaved: (value) {
+          emailController.text = value;
+        },
+        textInputAction: TextInputAction.next,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.mail),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Email",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+
+    final passField = TextFormField(
+        autofocus: false,
+        controller: passController,
+        obscureText: true,
+        //validator: () {},
+        onSaved: (value) {
+          passController.text = value;
+        },
+        textInputAction: TextInputAction.done,
+        decoration: InputDecoration(
+          prefixIcon: Icon(Icons.vpn_key),
+          contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+          hintText: "Password",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ));
+
+    final loginButton = Material(
+      elevation: 5,
+      borderRadius: BorderRadius.circular(30),
+      color: Colors.purpleAccent,
+      child: MaterialButton(
+        padding: EdgeInsets.fromLTRB(20, 15, 20, 15),
+        minWidth: MediaQuery.of(context).size.width,
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      bottom_navigation_bar.MyBottomNavigationBar()));
+        },
+        child: Text("Login",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            )),
+      ),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.purple,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.all(36.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    // SizedBox(
+                    //   height: 200,
+                    //   child: Image.asset(name, fit: BoxFit.contain, )
+                    // ),
+                    SizedBox(height: 45),
+                    emailField,
+                    SizedBox(height: 25),
+                    passField,
+                    SizedBox(height: 35),
+                    loginButton,
+                    SizedBox(height: 15),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text("Don't have an Account? "),
+                        GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => registration_screen
+                                          .RegistrationScreen()));
+                            },
+                            child: Text("SignUp",
+                                style: TextStyle(
+                                    color: Colors.purpleAccent,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 15))),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- womens_courtyard/lib/main.dart
+++ womens_courtyard/lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'personal_file_search_page.dart' as file_search_page;
-import 'Home_page.dart' as home_page;
-import 'Search_full_page.dart' as daily_search_page;
-import 'searchContact.dart' as view_contact;
 import 'package:firebase_core/firebase_core.dart';
 import 'dart:async';
+import 'login_screen.dart' as login_screen;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -40,63 +37,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Login",
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: MyBottomNavigationBar(),
-    );
-  }
-}
-
-class MyBottomNavigationBar extends StatefulWidget {
-  @override
-  _MyBottomNavigationBarState createState() => _MyBottomNavigationBarState();
-}
-
-class _MyBottomNavigationBarState extends State<MyBottomNavigationBar> {
-  int _currentIndex = 0;
-  final List<Widget> _children = [
-    home_page.MyHomePage(),
-    daily_search_page.MyHomePage(),
-    file_search_page.HomePage(),
-    view_contact.HomePage(),
-  ];
-
-  void onTappedBar(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      body: _children[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: onTappedBar,
-        currentIndex: _currentIndex,
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'מסך הבית',
-              backgroundColor: Colors.purpleAccent),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.calendar_today),
-              label: 'נוכחות יומית',
-              backgroundColor: Colors.purpleAccent),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.insert_drive_file),
-              label: 'תיקים',
-              backgroundColor: Colors.purpleAccent),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.account_box_rounded),
-              label: 'אנשי קשר',
-              backgroundColor: Colors.purpleAccent),
-        ],
-        backgroundColor: Colors.purpleAccent,
-        fixedColor: Colors.white,
-      ),
+      home: login_screen.LoginScreen(),
     );
   }
 }
