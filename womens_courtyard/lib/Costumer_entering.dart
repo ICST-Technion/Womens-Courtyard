39c9f242652e994fbfd4f31e31c5c1a620c95897
Itamar Juwiler <itamarj@campus.technion.ac.il>
2022-04-15 10:09:42 +0300

    minor build changes

--- womens_courtyard/lib/Costumer_entering.dart
+++ womens_courtyard/lib/Costumer_entering.dart
@@ -4,10 +4,6 @@ import 'BottomNavigationBar.dart' as bottom_navigation_bar;
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -146,12 +142,12 @@ class _MyHomePageState extends State<MyHomePage> {
         keyboardType: TextInputType.name,
         validator: (value) {
           if (value == null || value.isEmpty) {
-            return "הכניסי מספר פאלפון";
+            return "הכניסי מספר טלפון";
           }
           //regExp
           RegExp passReg = new RegExp(r'^(?:[+0]9)?[0-9]{10}$');
           if (!passReg.hasMatch(value)) {
-            return "הכניסי טלפון חוקי";
+            return "הכניסי מספר טלפון חוקי";
           }
           return null;
         },
@@ -162,7 +158,7 @@ class _MyHomePageState extends State<MyHomePage> {
         decoration: InputDecoration(
           prefixIcon: Icon(Icons.account_circle),
           contentPadding: EdgeInsets.fromLTRB(20, 15, 20, 15),
-          hintText: "מספר פאלפון",
+          hintText: "מספר טלפון",
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10),
           ),
@@ -194,127 +190,140 @@ class _MyHomePageState extends State<MyHomePage> {
       child: Scaffold(
         appBar: getHomepageAppBar(),
         body: Center(
-          child: ListView(
-            shrinkWrap: true,
-            padding: EdgeInsets.all(15.0),
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Text(
-                  'תיק טיפול',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-              Row(
-                children: <Widget>[
-                  Flexible(child: privateNameField),
-                  SizedBox(
-                    width: 20.0,
+          child: Form(
+            key: _formKey,
+            child: ListView(
+              shrinkWrap: true,
+              padding: EdgeInsets.all(15.0),
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Text(
+                    'תיק טיפול',
+                    style: TextStyle(fontSize: 20),
                   ),
-                  Flexible(child: fNameField),
-                ],
-              ),
-              SizedBox(
-                height: 150,
-                child: Flex(
-                  direction: Axis.vertical,
+                ),
+                Row(
                   children: <Widget>[
-                    Flexible(child: tazField),
+                    Flexible(child: privateNameField),
                     SizedBox(
-                      height: 10.0,
+                      width: 20.0,
                     ),
-                    Flexible(child: phoneNumberField),
-                    SizedBox(
-                      height: 10.0,
-                    ),
+                    Flexible(child: fNameField),
                   ],
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Text(
-                  'אנשי קשר:',
-                  style: TextStyle(fontSize: 20),
+                SizedBox(
+                  height: 150,
+                  child: Flex(
+                    direction: Axis.vertical,
+                    children: <Widget>[
+                      Flexible(child: tazField),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Flexible(child: phoneNumberField),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(40.0),
-                child: ElevatedButton(
-                    child: Text("הוספת איש קשר"),
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => add_contact_page.MyApp()));
-                    },
-                    style: ElevatedButton.styleFrom(
-                        primary: Color.fromRGBO(250, 84, 9, 0),
-                        elevation: 4,
-                        minimumSize: Size(10, 10),
-                        textStyle: TextStyle(color: Colors.white, fontSize: 20),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0)))),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Container(
-                  padding: EdgeInsets.symmetric(vertical: 12, horizontal: 8),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(24),
-                      boxShadow: [
-                        BoxShadow(blurRadius: 20, spreadRadius: -15)
-                      ]),
-                  child: proccessDescriptionField),
-              Padding(
-                padding: const EdgeInsets.all(40.0),
-                child: ElevatedButton(
-                    child: Text("הזנת קבצים חדשים"),
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                        primary: Color.fromRGBO(250, 84, 9, 0),
-                        elevation: 4,
-                        minimumSize: Size(100, 50),
-                        textStyle: TextStyle(color: Colors.white, fontSize: 20),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0)))),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(64.0),
-                child: ElevatedButton(
-                    child: Text("סיום ושמירה"),
-                    onPressed: () {
-                      //enter the results from the controllers.text into the firebase, then navigate back.
-                      // showDialog(
-                      //   context: context,
-                      //   builder: (context) {
-                      //     return AlertDialog(
-                      //       content: Text(nameTextController.text),
-                      //     );
-                      //   },
-                      // );
-                      enterFileToDatabase(
-                          nameTextController.text,
-                          fNameTextController.text,
-                          tazTextController.text,
-                          phoneNumberTextController.text,
-                          processDescriptionTextController.text);
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => bottom_navigation_bar
-                                  .MyBottomNavigationBar()));
-                    },
-                    style: ElevatedButton.styleFrom(
-                        primary: Color.fromRGBO(250, 84, 9, 0),
-                        elevation: 4,
-                        minimumSize: Size(150, 50),
-                        textStyle: TextStyle(color: Colors.white, fontSize: 20),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0)))),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    'אנשי קשר:',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(40.0),
+                  child: ElevatedButton(
+                      child: Text("הוספת איש קשר"),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    add_contact_page.MyApp()));
+                      },
+                      style: ElevatedButton.styleFrom(
+                          primary: Color.fromRGBO(250, 84, 9, 0),
+                          elevation: 4,
+                          minimumSize: Size(10, 10),
+                          textStyle:
+                              TextStyle(color: Colors.white, fontSize: 20),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0)))),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Container(
+                    padding: EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(24),
+                        boxShadow: [
+                          BoxShadow(blurRadius: 20, spreadRadius: -15)
+                        ]),
+                    child: proccessDescriptionField),
+                Padding(
+                  padding: const EdgeInsets.all(40.0),
+                  child: ElevatedButton(
+                      child: Text("הזנת קבצים חדשים"),
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          primary: Color.fromRGBO(250, 84, 9, 0),
+                          elevation: 4,
+                          minimumSize: Size(100, 50),
+                          textStyle:
+                              TextStyle(color: Colors.white, fontSize: 20),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0)))),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(64.0),
+                  child: ElevatedButton(
+                      child: Text("סיום ושמירה"),
+                      onPressed: () {
+                        //enter the results from the controllers.text into the firebase, then navigate back.
+                        // showDialog(
+                        //   context: context,
+                        //   builder: (context) {
+                        //     return AlertDialog(
+                        //       content: Text(nameTextController.text),
+                        //     );
+                        //   },
+                        // );
+                        if (_formKey.currentState != null &&
+                            _formKey.currentState.validate()) {
+                          enterFileToDatabase(
+                              nameTextController.text,
+                              fNameTextController.text,
+                              tazTextController.text,
+                              phoneNumberTextController.text,
+                              processDescriptionTextController.text);
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text("הכנסת תיק מוצלחת")),
+                          );
+                        }
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => bottom_navigation_bar
+                                    .MyBottomNavigationBar()));
+                      },
+                      style: ElevatedButton.styleFrom(
+                          primary: Color.fromRGBO(250, 84, 9, 0),
+                          elevation: 4,
+                          minimumSize: Size(150, 50),
+                          textStyle:
+                              TextStyle(color: Colors.white, fontSize: 20),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0)))),
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -324,17 +333,19 @@ class _MyHomePageState extends State<MyHomePage> {
   AppBar getHomepageAppBar() {
     return AppBar(title: Text('הזנת תיק אישי'), actions: [
       IconButton(
+          onPressed: () {},
           icon: Icon(
-        Icons.account_circle,
-        size: 30,
-        color: Colors.white,
-      )),
+            Icons.account_circle,
+            size: 30,
+            color: Colors.white,
+          )),
       IconButton(
+          onPressed: () {},
           icon: Icon(
-        Icons.info,
-        size: 30,
-        color: Colors.white,
-      )),
+            Icons.info,
+            size: 30,
+            color: Colors.white,
+          )),
     ]);
   }
 
